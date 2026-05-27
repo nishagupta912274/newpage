e924eb4a00d3a9bc41404f28fae89491ea3beb9c
nishagupta912274 <nishagupta912274@gmail.com>
2026-05-27 20:31:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-
+function fun(){
+    alert("Click Hua, Opening Google");
+    window.location="https://Google.com";
+}
+function fb(){
+    alert("Click Hua, Opening Facebook");
+    window.location="https://facebook.com";
+}
+function git(){
+    alert("Click Hua, Opening Github");
+    window.location="https://Github.com";
+}
